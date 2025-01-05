d8fa5d00a61f2b46ec878e43f620c43dee95abff
Iana Stelmakh <stelmakyana99@gmail.com>
2025-01-05 23:32:05 +1100

    Пример файла на Python

---
+++ emotions.py
@@ -0,0 +1,19 @@
+from graphviz import Digraph
+
+# Создаём направленный граф
+dot = Digraph()
+
+# Добавляем вершины
+dot.node('A', 'Node A')
+dot.node('B', 'Node B')
+dot.node('C', 'Node C')
+
+# Добавляем рёбра
+dot.edge('A', 'B')
+dot.edge('B', 'C')
+dot.edge('C', 'A')
+
+# Сохраняем граф
+dot.render('graph', format='png', cleanup=True)
+
+print("Граф сохранён как graph.png")
